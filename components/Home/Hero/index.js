d78991389c801dea437ad4fd6fb2323b0be390c8
jp-quintana <juanquintana1996@gmail.com>
2023-03-22 15:44:34 -0300

    Add Base Hero

--- components/Home/Hero/index.js
+++ components/Home/Hero/index.js
@@ -29,38 +29,40 @@ const Hero = () => {
               </a>
             </li>
           </ul>
-          <code className={styles.stack_container}>
-            <ul className={styles.icons}>
-              <li>
-                <img src="svgs/react.svg" alt="react" />
-              </li>
-              <li>
-                <img src="svgs/js.svg" alt="js" />
-              </li>
-              <li>
-                <img src="svgs/scss.svg" alt="scss" />
-              </li>
-              <li>
-                <img src="svgs/css.svg" alt="css" />
-              </li>
-              <li>
-                <img src="svgs/html.svg" alt="html" />
-              </li>
-              <li>
-                <img src="svgs/node.svg" alt="node" />
-              </li>
-              <li>
-                <img src="svgs/express.svg" alt="express" />
-              </li>
-              <li>
-                <img src="svgs/mongo.svg" alt="mongo" />
-              </li>
-              <li>
-                <img src="svgs/firebase.svg" alt="firebase" />
-              </li>
-            </ul>
-            <p className={styles.comment}>// stack</p>
-          </code>
+          {/* <div className={styles.stack_container}>
+            <div className={styles.stack_wrapper}>
+              <ul className={styles.icons}>
+                <li>
+                  <img src="svgs/react.svg" alt="react" />
+                </li>
+                <li>
+                  <img src="svgs/js.svg" alt="js" />
+                </li>
+                <li>
+                  <img src="svgs/scss.svg" alt="scss" />
+                </li>
+                <li>
+                  <img src="svgs/css.svg" alt="css" />
+                </li>
+                <li>
+                  <img src="svgs/html.svg" alt="html" />
+                </li>
+                <li>
+                  <img src="svgs/node.svg" alt="node" />
+                </li>
+                <li>
+                  <img src="svgs/express.svg" alt="express" />
+                </li>
+                <li>
+                  <img src="svgs/mongo.svg" alt="mongo" />
+                </li>
+                <li>
+                  <img src="svgs/firebase.svg" alt="firebase" />
+                </li>
+              </ul>
+              <p className={styles.comment}>// stack</p>
+            </div>
+          </div> */}
         </div>
       </div>
     </section>
