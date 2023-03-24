c8e5964186afb859a37b22cb81d69db52c5567ae
jp-quintana <juanquintana1996@gmail.com>
2023-03-24 04:24:20 -0300

    Update Nav

--- components/Layout/Nav/index.js
+++ components/Layout/Nav/index.js
@@ -1,9 +1,43 @@
+import { useContext, useEffect, useState } from 'react';
+// import ScrollContext from './ScrollContext';
 import styles from './index.module.scss';
 
 const Nav = () => {
+  // const scrollContext = useContext(ScrollContext);
+  const [heroIsGone, setHeroIsGone] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const heroHeight = window.innerHeight;
+      const scrollPosition = document.documentElement.scrollTop;
+
+      if (scrollPosition >= heroHeight) {
+        setHeroIsGone(true);
+      } else {
+        setHeroIsGone(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  console.log(heroIsGone);
+
   return (
-    <div className={styles.container}>
-      <nav className={`${styles.nav} main-container`}>
+    <div
+      className={`${styles.container} ${
+        heroIsGone ? styles.container_rest : ''
+      }`}
+    >
+      <nav
+        className={`${styles.nav} ${
+          heroIsGone ? styles.nav_rest : ''
+        } main-container`}
+      >
         <div className={styles.logo}>jpquintana.dev</div>
         <ul className={styles.list}>
           <li>Home</li>
