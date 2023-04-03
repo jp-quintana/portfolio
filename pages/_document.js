bd2fce223bc995d8794a1e388c77f31729888d04
jp-quintana <juanquintana1996@gmail.com>
2023-04-02 23:58:09 -0300

    WIP2: SideNav

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+
       <body>
+        <div id="overlay"></div>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
