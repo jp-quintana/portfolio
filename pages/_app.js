b237fc6a940cc895cd800da89176d52378784b7a
jp-quintana <juanquintana1996@gmail.com>
2023-04-11 01:42:30 -0300

    Update Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
+import Head from 'next/head';
+
 import Layout from '@/components/Layout';
 
 import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
