e5c9f7603db03766beeaffecdb1731b1b594cc80
jp-quintana <juanquintana1996@gmail.com>
2023-04-12 00:52:29 -0300

    Update Font

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,23 @@
 import Head from 'next/head';
+import { Inter } from '@next/font/google';
 
 import Layout from '@/components/Layout';
 
 import '@/styles/globals.scss';
 
+const inter = Inter({
+  subsets: ['latin'],
+});
+
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <div className={inter.className}>
       <Head>
         <title>Home</title>
       </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </div>
   );
 }
